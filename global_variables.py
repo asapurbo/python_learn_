5910955685390da3ba9423482b5fa8f5b7ab1bda
Apurbo Howlader <cstapurbo@gmail.com>
2026-02-02 21:30:59 +0600

    done

---
+++ global_variables.py
@@ -0,0 +1,13 @@
+# global variable
+x = "nice"
+
+def my_demo():
+    # local variable
+    y = "awesome"
+    print('1', x)
+    print('2', y)
+
+
+my_demo()
+
+print('3', x)
--- main.py
+++ main.py
@@ -1,23 +1,97 @@
-x = 3
-y = 5
-a = 98
+# 1. A variable name must start with a letter or the underscore character
+_name = 5
+Name = 5
+name = 5
 
-# ============
-r = 487
-h = 984
-o = 94587
-# =============
-i = 5745
-c = 9347
-b = 43554
-p = 9484
+# 2. A variable name cannot start with a number
+# 2name = 4
 
-v = x + y + a
+# 3. A variable name can only contain alpha-numeric charactes and underscores (A-Z, a-z, 0-9, _)
+_na768me34 = 76
 
-lk = r + h + o
-ui = i + c + b + p
+# 4. Variable names are cas-sensitive (age, Age, AGE)
+age = 12
+Age = 13
+AGE = 14
 
-sum_x = v + lk + ui
+# 5. A variable name cannot be any of the Python Keywords
+# and = 'apurbo'
 
-print('v', v)
+
+# example (legal variable names):
+myname = 'Apurbo'
+my_name = 'Apurbo'
+_my_name = 'Apurbo'
+myName = 'Apurbo'
+MYNAME = 'Apurbo'
+myname3 = 'Apurbo'
+
+
+# print(myname)
+# print(my_name)
+# print(_my_name)
+# print(myName)
+# print(MYNAME)
+# print(myname3)
+
+
+# example (illegal variable names):
+# 2myname = 'Apurbo'
+# my-name = 'Apurbo'
+# my name = 'Apurbo'
+
+# print(2myname)
+# print(my-name)
+# print(my name)
+
+
+
+# multi words variable Names
+# myname = 'Apurbo'
+
+# 1. Camel Case
+myName = 'Apurbo'
+
+# 2. Pascal Case
+MyName = 'Apurbo'
+
+# 3. Snake Case
+my_name = 'Apurbo'
+
+
+
+# many values to multiple variables
+x, y, z = 'rohim', 'korim', 'kamal'
+
+# print(x)
+# print(y)
+# print(z)
+
+x = y = z = "rohim"
+
+# print(x)
+# print(y)
+# print(z)
+
+# 'rohim', 'korim', 'kamal'
+# list
+x = ['rohim', 'korim', 'kamal']
+
+a, b, c = x
+
+# print(a, b, c)
+
+# end(' ')
+x = 'apple'
+y = 'banana'
+z = 'cherry'
+# apple, banana, cherry
+
+# \n
+
+# print(x, end='\n')
+# print(y, end=', ')
+# print(z)
+
+
 
