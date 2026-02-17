2b303a5df7a9650e8c7f3e6594ae4d2c41f2dcb5
Apurbo Howlader <cstapurbo@gmail.com>
2026-02-17 21:07:17 +0600

    done

---
+++ string.py
@@ -0,0 +1,68 @@
+# name = "I'm Apurbo"
+
+# print(name)
+
+
+'''
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+'''
+
+
+"""
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
+
+
+
+
+
+
+
+
+
+
+
+# food = "PIZZA"
+
+# for i in food:
+#     print('# ', i)
+#     print('= ', i)
+
+
+
+
+# txt = "The best things in life are free!"
+
+# in
+
+# x = 'of' in txt
+# print(x)
+
+# =
+# ==
+
+# txt = "The best things in life are free!"
+
+# if 'of' not in txt:
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+
+# x = not False
+
+# print(x)
+
+
+
+txt = "The best things in life are free!"
+
+if 'best' not in txt:
+    print('yes')
+else:
+    print('no')
+
+
